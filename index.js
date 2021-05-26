b8d27575732faa9793d5981e5af3499c4660086e
JavaScythe <>
2021-05-26 20:52:06 +0000

    peernet conn v3

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 //https://replit.com/@Haxxors/coinws
 const APP_VERSION = "0.0.1";
 var peernet = require("socket.io-client").connect("https://coinws.haxxors.repl.co", {reconnect: true});
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+const axios = require('axios');
 const fs = require("fs");
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.get('/',(req, res) => {
+  res.sendFile("/index.html");
+});
+app.post('/block',(req, res) => {
+  console.log(req.body);
+  res.send(JSON.stringify(req.body)+"<1");
+});
+const client = (process.env.REPL_SLUG+"."+process.env.REPL_OWNER+".repl.co").toLowerCase();
 peernet.on("ping", (data => {
   console.log(data);
 }));
@@ -22,12 +35,17 @@ fs.readFile("self.json", async function(err, data){
     console.log("peer exists");
     isRequesting=1;
     requestResponse = [];
-    peernet.emit("chainrequest", data);
+    //peernet.emit("chainrequest", data);
+    await delay(1000);
+    console.log(requestResponse);
   }
 });
 peernet.on("chainrequest", (data => {
   if(isRequesting==0){
-    peernet.emit("chainrequestres", BlockChain);
+    axios.post("https://"+data["fullurl"]+"/block", {
+      sender: client,
+      data: BlockChain
+    });
   }
 }));
 peernet.on("chainrequestres", (data => {
@@ -36,3 +54,4 @@ peernet.on("chainrequestres", (data => {
     console.log(requestResponse);
   }
 }));
+app.listen(8080,() => {});
